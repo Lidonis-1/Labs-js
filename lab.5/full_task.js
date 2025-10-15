a78857d46b9d8e5fa5ac9cef56f0303a3c7bfd47
Lidonis-1 <prototup02@gmail.com>
2025-10-15 20:04:59 +0300

    .

--- lab.5/full_task.js
+++ lab.5/full_task.js
@@ -26,7 +26,7 @@ console.log(seq(x => x + 1)
 
 
 
-
+   
 const array = () => {
    let list = [];
 
---
+++ lab.6/full.task.js
@@ -0,0 +1,52 @@
+`use strict`
+
+{
+const pipe = (...funcs) => {
+    if (funcs.some(x => typeof x !== `function`)){
+        throw  `there is a parameter which not a function`;
+    }
+
+    return (a) => funcs.reduce((acc, item) => item(acc), a);
+    
+}
+    
+const inc = x => ++x;
+const twice = x => x * 2;
+const cube = x => x ** 3;
+
+const ex = pipe(inc, twice, cube);
+console.log(ex(5));
+
+}
+
+
+{
+const pipe = (...funcs) => {
+    let er = '';
+    let base = (num)=>{
+        return (funcs.toReversed()).reduce((acc, item) => item(acc), num);
+    }
+    try {
+        base(5);
+    } catch (eror){
+        er = eror ;
+        base = 'und';
+    }
+    base.on = ()=>{
+        base = er ;
+    }
+    return base;
+}
+    
+const inc = x => ++x;
+const twice = x => x * 2;
+const cube = x => x ** 3;
+
+const ex = pipe(inc, twice, cube);
+console.log(ex(5));
+
+const ex_2 = pipe(inc, 7,twice);
+console.log(ex_2);
+
+console.log(ex_2.on);
+}

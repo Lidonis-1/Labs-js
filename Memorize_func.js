5cd5fb40f32265ae5fcef438c2a2188084fcdfe8
Lidonis-1 <prototup02@gmail.com>
2026-04-07 22:27:01 +0300

    start work on lab_4

--- Memorize_func.js
+++ Memorize_func.js
@@ -4,34 +4,46 @@ const generateKey = (args) => args.map(argKey).join('|');
 
 const sObj=(obj, last)=>{
     const keys = Object.keys(obj);          
-    const lastKeys = keys.slice(-last + 1);  
+    const lastKeys = keys.slice(-last);  
 
     for (const key of keys){
       if (!lastKeys.includes(key)){
-        console.log("delete :" + obj[key])
         delete obj[key]
+        console.dir(obj)
+
       }
     }
 }
 
 
-const memoize = (fn) => {
+const memoize = (fn, max_size, ttl) => {
   let cache = Object.create(null);
+  ttl = ttl*1000
 
   const iFunc = (...args) => {
     const key = generateKey(args);
-
-    if (key in cache) return cache[key];
+    const time = new Date()
+    if (key in cache) {
+      if(new Date()- time< ttl){
+        return cache[key].res
+      }
+      else{
+        delete cache[key]
+      }
+    }
     const res = fn(iFunc, ...args);
-    cache[key] = res;
-    //console.dir(cache)
-    sObj(cache,4)
+    cache[key] = {res, time};
+    sObj(cache, max_size)
+    console.dir(cache)
     return res;
   };
   return iFunc;
 };
 
 const fib = (f, n) => (n <= 2 ? 1 : f(n - 1) + f(n - 2));
-const mFib = memoize(fib);
+const mFib = memoize(fib, 3, 6);
 
-console.log(mFib(100))
+console.log(mFib(3))
+console.log(mFib(1))
+console.log(mFib(4))
+
---
+++ queue.js
@@ -0,0 +1,19 @@
+'use strict'
+
+const queue={
+    "q": [{"item": "", "priority": 0}],
+    "enqueue":(item, priorety)=>{this.q.},
+    "dequeue":(cas)=>{
+        switch(a){ //"highest, lowest, oldest, newest"
+            case "highest":
+                this.q.;
+            case "lowest":
+            case "oldest":
+            case "newest":
+        }
+        
+    }
+}
+queue.enqueue("lol")
+
+

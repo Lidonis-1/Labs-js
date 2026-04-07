fb9eead461a30603a6567060b5da71f95dc1e913
Lidonis-1 <prototup02@gmail.com>
2026-04-07 23:22:35 +0300

    add logic of dequeue (queue.js)

--- queue.js
+++ queue.js
@@ -12,19 +12,28 @@ const queue={
         }
         this.q_sort_priorety.splice(i+1, 0 , element)
     },
-    "dequeue":(cas)=>{
+    dequeue(cas){
+        let element;
         switch(a){ //"highest, lowest, oldest, newest"
             case "highest":
+                element = this.q_sort_priorety[this.q_sort_priorety.length - 1]
             case "lowest":
+                element = this.q_sort_priorety[0]
             case "oldest":
+                element = this.q_sort_timeAdd[0]
             case "newest":
+                element = this.q_sort_timeAdd[this.q_sort_ti.length - 1]
         }
+        this.q_sort_timeAdd = this.q_sort_timeAdd.filter(e => e !== element);
+        this.q_sort_priorety = this.q_sort_priorety.filter(e => e !== element);
+    },
+    peek(){
         
     }
 }
 queue.enqueue("pingvin", 5);
 queue.enqueue("radic", 3);
 queue.enqueue("dog", 7);
-console.log(JSON.stringify(queue.q_sort_priorety));
+console.table(queue.q_sort_priorety);
 console.dir(JSON.stringify(queue.q_sort_timeAdd));
 

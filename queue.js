78b0866b6688cac047efe614ceb2afdb0969d771
Lidonis-1 <prototup02@gmail.com>
2026-04-07 22:55:37 +0300

    fix enqueue

--- queue.js
+++ queue.js
@@ -1,16 +1,16 @@
 'use strict'
 
 const queue={
-    q_sort_priority: [],
+    q_sort_priorety: [],
     q_sort_timeAdd: [],
-    "enqueue":(item, priorety)=>{
+    enqueue(item, priorety){
         const element = {item, priorety};
         this.q_sort_timeAdd.push(element);
-        let i = this.q_sort_priority.length;
-        while( i>=0 && this.q_sort_priority[i].priority> priorety){
+        let i = this.q_sort_priorety.length -1;
+        while( i>=0 && this.q_sort_priorety[i].priorety > priorety){
             i--;
         }
-        this.q_sort_priority.splice(i+1, 0 , element)
+        this.q_sort_priorety.splice(i+1, 0 , element)
     },
     "dequeue":(cas)=>{
         switch(a){ //"highest, lowest, oldest, newest"
@@ -25,6 +25,6 @@ const queue={
 queue.enqueue("pingvin", 5);
 queue.enqueue("radic", 3);
 queue.enqueue("dog", 7);
-console.dir(queue.q_sort_priority);
-console.dir(queue.q_sort_timeAdd);
+console.log(JSON.stringify(queue.q_sort_priorety));
+console.dir(JSON.stringify(queue.q_sort_timeAdd));
 

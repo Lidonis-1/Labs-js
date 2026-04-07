d91975367e1f6c1890cd0aff9afb68a3c8a0c5a6
Lidonis-1 <prototup02@gmail.com>
2026-04-07 22:40:56 +0300

    add logic of enqueue

--- queue.js
+++ queue.js
@@ -1,12 +1,20 @@
 'use strict'
 
 const queue={
-    "q": [{"item": "", "priority": 0}],
-    "enqueue":(item, priorety)=>{this.q.},
+    q_sort_priority: [],
+    q_sort_timeAdd: [],
+    "enqueue":(item, priorety)=>{
+        const element = {item, priorety};
+        this.q_sort_timeAdd.push(element);
+        let i = this.q_sort_priority.length;
+        while( i>=0 && this.q_sort_priority[i].priority> priorety){
+            i--;
+        }
+        this.q_sort_priority.splice(i+1, 0 , element)
+    },
     "dequeue":(cas)=>{
         switch(a){ //"highest, lowest, oldest, newest"
             case "highest":
-                this.q.;
             case "lowest":
             case "oldest":
             case "newest":
@@ -14,6 +22,9 @@ const queue={
         
     }
 }
-queue.enqueue("lol")
-
+queue.enqueue("pingvin", 5);
+queue.enqueue("radic", 3);
+queue.enqueue("dog", 7);
+console.dir(queue.q_sort_priority);
+console.dir(queue.q_sort_timeAdd);
 

3fba089065b12569e682c1b447b6629eb1630ca9
Lidonis-1 <prototup02@gmail.com>
2026-04-07 23:23:23 +0300

    fix dequeueu (queue)

--- queue.js
+++ queue.js
@@ -14,7 +14,7 @@ const queue={
     },
     dequeue(cas){
         let element;
-        switch(a){ //"highest, lowest, oldest, newest"
+        switch(cas){ //"highest, lowest, oldest, newest"
             case "highest":
                 element = this.q_sort_priorety[this.q_sort_priorety.length - 1]
             case "lowest":
@@ -22,7 +22,7 @@ const queue={
             case "oldest":
                 element = this.q_sort_timeAdd[0]
             case "newest":
-                element = this.q_sort_timeAdd[this.q_sort_ti.length - 1]
+                element = this.q_sort_timeAdd[this.q_sort_timeAdd.length - 1]
         }
         this.q_sort_timeAdd = this.q_sort_timeAdd.filter(e => e !== element);
         this.q_sort_priorety = this.q_sort_priorety.filter(e => e !== element);
@@ -34,6 +34,13 @@ const queue={
 queue.enqueue("pingvin", 5);
 queue.enqueue("radic", 3);
 queue.enqueue("dog", 7);
+queue.enqueue("cat", 6);
+queue.enqueue("pig", 7);
+queue.enqueue("colibri", 9);
+queue.enqueue("fog", 2);
 console.table(queue.q_sort_priorety);
-console.dir(JSON.stringify(queue.q_sort_timeAdd));
-
+console.table(queue.q_sort_timeAdd);
+queue.dequeue("highest")
+queue.dequeue("oldest")
+console.table(queue.q_sort_priorety);
+console.table(queue.q_sort_timeAdd);
